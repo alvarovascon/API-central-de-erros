80731e4f37197907be6431d364822f5abdebe9cd
alvarovascon <alvarovascon2@gmail.com>
2021-05-01 19:08:33 -0300

    cors - 2a tentativa

--- demo/src/main/java/com/central/main/security/WebConfig.java
+++ demo/src/main/java/com/central/main/security/WebConfig.java
@@ -4,11 +4,11 @@ package com.central.main.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
