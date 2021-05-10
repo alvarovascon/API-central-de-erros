0dcf9681d180384c5f6060ad48d320e5d840a45c
Israel Menon <zezomenon@gmail.com>
2021-05-10 11:11:36 -0300

    Testes UserServices e arquivos mock e sql

--- src/test/java/com/central/main/suite/UserServiceTest.java
+++ src/test/java/com/central/main/suite/UserServiceTest.java
@@ -1,45 +1,77 @@
 package com.central.main.suite;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.central.main.entity.User;
-import com.central.main.repository.UserRepository;
 import com.central.main.service.UserService;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@DataJpaTest
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Sql({"/user_service_test.sql"})
 @ActiveProfiles("test")
 public class UserServiceTest {
     private static final String EMAIL = "admin@admin.com";
+    private static final String PASSWORD = "1234567";
 
     @Autowired
     private UserService userService;
+
+    public UserServiceTest() {
+    }
+
+    private User getUser() {
+        User user = new User();
+        user.setEmail("admin@admin.com");
+        user.setPassword("1234567");
+        return user;
+    }
+
+    private void assertUser(User result) {
+        Assert.assertThat(result.getId(), Matchers.notNullValue());
+        Assert.assertThat(result.getEmail(), Matchers.equalTo(EMAIL));
+        Assert.assertThat(result.getPassword(), Matchers.equalTo(PASSWORD));
+    }
 
     @Test
     @Transactional
     public void whenSave() {
         User user = this.getUser();
         User result = this.userService.save(user);
-//        this.(result);
+        this.assertUser(result);
     }
 
-//    @Test
-//    public void testFindByEmail(User email) throws Exception {
-//        assertThat(userService).isNotNull();
-//        assertThat(email.getEmail(), Matchers.equalTo("admin@admin.com"));
-//
-//    }
+    @Test
+    @Transactional
+    public void whenFindById() {
+        Optional<User> result = this.userService.findById(AbstractTest.UserIds.ONE);
+        Assert.assertThat(result.isPresent(), Matchers.equalTo(true));
+        this.assertUser(result.get());
+    }
 
+    @Test
+    @Transactional
+    public void whenFindByEmail() {
+        Optional<User> result = this.userService.findByEmail(AbstractTest.UserEmails.EMAIL_ONE);
+        Assert.assertThat(result.isPresent(), Matchers.equalTo(true));
+        this.assertUser(result.get());
+    }
 
-    private User getUser() {
-        User user = new User();
-        user.setEmail("admin@admin.com");
-        return user;
+    @Test
+    @Transactional
+    public void whenFindAll() {
+        List<User> result = this.userService.findAll();
+        Assert.assertThat(result, Matchers.hasSize(5));
     }
 
 }
---
+++ src/test/resources/user_service_test.sql
@@ -0,0 +1,17 @@
+delete
+from users;
+
+insert into users (id, email, password)
+values (1, 'admin@admin.com', '1234567');
+
+insert into users (id, email, password)
+values (2, 'alvaro@gmail.com', '123');
+
+insert into users (id, email, password)
+values (3, 'israel@gmail.com', '1234');
+
+insert into users (id, email, password)
+values (4, 'leticia@gmail.com', '12345');
+
+insert into users (id, email, password)
+values (5, 'rodolfo@gmail.com', '123456');
